2988de94cc122e2e11d106e989361b84a77c4226
greenorb007 <56039777+greenorb007@users.noreply.github.com>
2022-10-31 18:15:46 +0530

    Add essential macros for the driver.
    Add essential macros.

--- pr_deb_module.c
+++ pr_deb_module.c
@@ -1,14 +1,18 @@
+#define MODULE
+#define LINUX
+#define __KERNEL__
+
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
 
-static int load_fun(void)
+static int __init load_fun(void)
 {
 	pr_debug("Hello, World  kernel :) \n");
 	return 0;
 }
 
-static void unload_fun(void)
+static void __exit unload_fun(void)
 {
 	pr_debug("unloading...\n");
 }
